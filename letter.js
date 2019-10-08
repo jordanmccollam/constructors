8a8f2c3508472b3e398804f0a2fd673d5dbc8317
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-08 11:04:25 -0500

    Initial push

---
+++ letter.js
@@ -0,0 +1,8 @@
+// This file will contain a constructor (Letter). Letter will display an underlying character or blank placeholder depending on whether or not the user has guessed a letter.
+
+// Constructor should define...
+    // String value to store the underlying character for the letter
+    // A boolean that stores whether the letter has been guessed
+    // Function that returns the character if guessed, or placeholder if not guessed
+    // Function that takes a char at an argument and checks it against the underlying char, updating the boolean
+
